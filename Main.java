ce7c279e4554b79b818dbcabc57274aaeec52cb2
gudkovst <69453922+gudkovst@users.noreply.github.com>
2023-05-15 00:02:55 +0800

    init

---
+++ Main.java
@@ -0,0 +1,11 @@
+package application;
+
+import view.IView;
+import view.ViewSwing;
+
+public class Main {
+    public static void main(String[] args) {
+        IView view = new ViewSwing();
+        view.start();
+    }
+}
