f2d970afdf94148bfc75d34d030714e5529e92ef
gudkovst <69453922+gudkovst@users.noreply.github.com>
2023-05-15 13:07:14 +0700

    Update Config.java

--- src/main/java/config/Config.java
+++ src/main/java/config/Config.java
@@ -10,5 +10,5 @@ public class Config {
     public static String librarianPass = "root";
     public static String serverURL = "//84.237.50.81:8080/apex:";
     public static String serverLogin = "20206_Gudkov";
-    public static String serverPass = "682318";
+    public static String serverPass = "";
 }
