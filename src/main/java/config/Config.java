d58ef6c1945b4ce39e5a2110f54251ee01a86671
gudkovst <69453922+gudkovst@users.noreply.github.com>
2023-06-02 16:28:13 +0700

    add some features

--- src/main/java/config/Config.java
+++ src/main/java/config/Config.java
@@ -6,9 +6,11 @@ public class Config {
     public static int WIDTH = 600;
     public static int HEIGHT = 600;
     public static Color color = Color.white;
+    public static String[] tableNames = new String[]{"Books", "ContentBooks", "Composition", "Halls",
+            "TakeRegistration", "ReturnRegistration", "Readers"};
     public static String librarianLogin = "root";
     public static String librarianPass = "root";
     public static String serverURL = "jdbc:oracle:thin:@84.237.50.81:1521:XE";
     public static String serverLogin = "20206_Gudkov";
-    public static String serverPass = "";
+    public static String serverPass = "682318";
 }
